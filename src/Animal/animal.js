efd80fc6f45cab67c4d13588079b6c3c3162d3c6
rosanaemidio <rosanaemidio42@gmail.com>
2022-12-08 00:49:34 -0300

    feat: add class animal e arquivo de teste

---
+++ src/Animal/animal.js
@@ -0,0 +1,12 @@
+class Animal{
+    constructor(type, name, age, family, color, humor){
+        this.type = type;
+        this.name = name;
+        this.age = age;
+        this.family = family;
+        this.color = color;
+        this.humor = humor;
+    }
+}
+
+module.exports = Animal;
