e270fabaad070c3d342300fd3df3db6cfc218d10
rosanaemidio <rosanaemidio42@gmail.com>
2022-12-08 00:29:47 -0300

    feat: criado classe e teste adotante

---
+++ src/Adotante/adotante.js
@@ -0,0 +1,16 @@
+
+class Adotante {
+    constructor(name, age, profission, status, moradia){
+        this.name = name;
+        this.age = age;
+        this.profission = profission;
+        this.status = status;
+        this.moradia = moradia;
+    }
+
+    // validaDados(name, age, profission, moradia){
+    //     if(age)
+    // }
+}
+
+module.exports = Adotante;
